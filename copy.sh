8b9f0df014bf1dfc3aee3b60ece8701675c280ec
Daniel Murphy <dmurph@chromium.org>
2019-09-07 15:25:30 -0700

    Created better copy script, checks for existance of sourcing before appending to the file

--- copy.sh
+++ copy.sh
@@ -2,22 +2,46 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-echo "Installing dotfiles from $DIR to $HOME"
+echo -e "Installing dotfiles from $DIR to $HOME\n"
 
-echo "bash_profile"
-echo -e "\nsource \"$DIR/bash_profile\"" >> "$HOME/.bash_profile"
-echo "chromium_profile"
-echo -e "source \"$DIR/chromium_profile\"" >> "$HOME/.bash_profile"
-echo ".gitconfig"
-ln -sf "$DIR/git-config" ~/.gitconfig
-echo ".vimrc"
-echo -e "\nsource $DIR/vimrc >> "$HOME/.vimrc"
+BASH_PROFILE_FILE="$HOME/.bash_profile"
+BASH_PROFILE_STRING="source \"$DIR/bash_profile\""
+CHROMIUM_PROFILE_STRING="source \"$DIR/chromium_profile\""
+VIMRC_FILE="$HOME/.vimrc"
+VIMRC_STRING="source $DIR/vimrc"
 
-echo "Done"
+echo "bash_profile..."
+if grep -q "$BASH_PROFILE_STRING" $BASH_PROFILE_FILE; then
+  echo -e "bash_profile already sourced in $BASH_PROFILE_FILE\n"
+else
+  echo $BASH_PROFILE_STRING >> $BASH_PROFILE_FILE
+  echo -e "done.\n"
+fi
+
+echo "chromium_profile..."
+if grep -q "$CHROMIUM_PROFILE_STRING" $BASH_PROFILE_FILE; then
+  echo -e "chromium_profile already sourced in $BASH_PROFILE_FILE.\n"
+else
+  echo $CHROMIUM_PROFILE_STRING >> $BASH_PROFILE_FILE
+  echo -e "done.\n"
+fi
+
+echo ".gitconfig..."
+ln -sf "$DIR/git-config" $HOME/.gitconfig
+echo -e "done.\n"
+echo ".vimrc..."
+if grep -q "$VIMRC_STRING" $VIMRC_FILE; then
+  echo -e "vimrc already sourced in $VIMRC_FILE.\n"
+else
+  echo $VIMRC_STRING >> $VIMRC_FILE
+  echo -e "done.\n"
+fi
+echo "Complete."
 
 echo ""
 echo "Make sure to create a:"
 echo " * $DIR/gitconfig.local to store the username & email address."
+echo "   (for example, "
 echo " * $DIR/ps1-shortener.local to add shortened paths"
 echo ""
 
